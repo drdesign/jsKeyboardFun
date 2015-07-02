f98f86e76f9e546775ac14824ec04e6c9273ac02
Danniel Rolfe <info@dannielrolfe.com>
2015-07-02 13:47:55 -0700

    methods - headache

--- mainFactory.js
+++ mainFactory.js
@@ -7,7 +7,8 @@ var makeKeyboard =
 			return null;
 
 			var keyboard = {
-				keys : keys,
+				key : key, //key property
+				//shared methods
 				shift : shift,
 				capsLock : capsLock
 				
@@ -18,27 +19,27 @@ var makeKeyboard =
 
 //Resources
 		var keyName = [
-					  //alphabet	
-					  'a','A','b', 'B','c', 'C','d', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 
-	                  'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W','x', 'X', 'y', 'Y', 'z', 'Z', 
-	                  //numbers 
-	                  '1', '!', '2', '@', '3', '4', '$', '5', '%', '6', '^', '7', '&', '8', '*', '9', '(', '0', ')', 
-	                  //Special Keys
-	                  '-', '_', '=', '+','[','{',']', '}', '\\', '|',';', ':',"'",'"',',','<', '.', '>','/', '?', 
-	                  ];
+						  //alphabet	
+						  'a','A','b', 'B','c', 'C','d', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 
+		                  'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W','x', 'X', 'y', 'Y', 'z', 'Z', 
+		                  //numbers 
+		                  '1', '!', '2', '@', '3', '4', '$', '5', '%', '6', '^', '7', '&', '8', '*', '9', '(', '0', ')', 
+		                  //Special Keys
+		                  '-', '_', '=', '+','[','{',']', '}', '\\', '|',';', ':',"'",'"',',','<', '.', '>','/', '?', 
+	                	];
 //Methods
 
 		var keys = function(key){
 			var keyIndex = keyName.indexOf(key);
-			return keyName[key];
+			return keyName[keyIndex];
 		};
 		var shift  = function(key){
 			var keyIndex = keyName.indexOf(key);
-			return keyName[key + 1];
+			return keyName[keyIndex + 1];
 		};
 		var capsLock  = function(key){
 			var keyIndex = keyName.indexOf(key);
-			return keyName[key + 1];
+			return keyName[keyIndex + 1];
 		};
 
 
