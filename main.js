128695356629773fad9db7e8dd69afcd4fc248a4
Danniel Rolfe <info@dannielrolfe.com>
2015-07-07 12:29:26 -0700

    worked on the key function

--- main.js
+++ main.js
@@ -4,8 +4,11 @@
 
 	var keyboard = {
 		array : [
-				  	//alphabet	
-				  	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s', 't','u','v','w','x','y','z', 
+				  	// //alphabet	
+				  	// 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s', 't','u','v','w','x','y','z', 
+	          		//alphabet	
+				  	'a','A','b', 'B','c', 'C','d', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 
+	          		'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W','x', 'X', 'y', 'Y', 'z', 'Z', 
 	          		//numbers 
 	              	'1', '!', '2', '@', '3', '4', '$', '5', '%', '6', '^', '7', '&', '8', '*', '9', '(', '0', ')', 
 	          		//Special Keys
@@ -20,7 +23,7 @@
 		shift : function(key){
 				var keyIndex = this.array.indexOf(key)
 
-				if(keyIndex > 25){
+				if(keyIndex > 50){
 					var item = this.array[keyIndex];
 					console.log(keyIndex);
 					return this.array[keyIndex + 1];
@@ -34,7 +37,7 @@
 		capsLock : function(key){
 				var keyIndex = this.array.indexOf(key)
 
-				if(keyIndex > 25){
+				if(keyIndex > 50){
 					var item = this.array[keyIndex];
 					console.log(keyIndex);
 					return this.array[keyIndex + 1];
@@ -45,7 +48,12 @@
 					return res;
 				}
 			},
-		type : function(){
+		type : function(key){
+				var keyIndex = this.array.indexOf(key)
+				var item = this.array[keyIndex];
+				var word = [];
+				var addWord = word.push(key);
+				return word;
 
 		}
 	};
